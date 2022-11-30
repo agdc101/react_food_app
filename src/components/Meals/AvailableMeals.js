555ba3d3b9399a3d747d9d1850db70de78a7c011
agdc101 <agdc101@gmail.com>
2022-11-30 12:04:28 +0000

    input component added + label bug fix

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -37,7 +37,7 @@ const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map(meal => {
        return (
         <Card>
-          <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>
+          <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>
         </Card>
        )
     });
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from "react";
-import classes from '../MealItem/MealItem.module.css';
+import MealItemForm from "./MealItemForm";
+import classes from './MealItem.module.css';
 
 const MealItem = props => {
     const price = `$${props.price.toFixed(2)}`;
@@ -11,7 +12,7 @@ const MealItem = props => {
                 <p className={classes.description}>{props.description}</p>
                 <p className={classes.price}>{price}</p>
             </div>
-            <div>{}</div>
+            <div><MealItemForm id={props.id}/></div>
         </li>
     )
 }
