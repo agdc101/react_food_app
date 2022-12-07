454334340cefb4749fa8d388ae307fb9221bd08e
agdc101 <agdc101@gmail.com>
2022-12-07 14:34:53 +0000

    overlay functionality fix

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
         <Fragment>
             <header className={classes.header}>
                 <h1>Chinese Takeaway</h1>
-                <HeaderCartButton/>
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={headerImage} alt="a table full of tasty food."/>
