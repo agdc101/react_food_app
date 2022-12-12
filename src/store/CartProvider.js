b3d6eab2e13138659356adf425b239c86aede2cb
agdc101 <agdc101@gmail.com>
2022-12-12 11:19:17 +0000

    cart reducer - app needs fix

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, {useReducer} from "react";
 import CartContext from "./Cart-context";
 
-const CartProvider = (props) => {
-    const addItemHandler = (item) => {
+const defaultCartState = {
+    items: [],
+    totalAmount:0
+};
 
+const CartReducer = (state, action) => {
+    if (action.type === "ADD_ITEM") {
+        const updatedCart = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedCart,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    return defaultCartState;
+};
+
+const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(CartReducer, defaultCartState);
+
+    const addItemHandler = (item) => {
+        dispatchCartAction({type: "ADD_ITEM", item: item});
     };
 
     const removeItemHandler = (id) => {
-
+        dispatchCartAction({type: "REMOVE_ITEM", id: id});
     };
 
     const cartContext = {
-        items:[],
-        totalAmount:0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemHandler,
         removeItem: removeItemHandler
     };
