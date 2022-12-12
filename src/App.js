a4dd9ecce76c70b7cde95eff6ab5e97620de944a
agdc101 <agdc101@gmail.com>
2022-12-12 10:31:54 +0000

    store directory added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import CartProvider from "./store/CartProvider";
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals';
 import Cart from "./components/Cart/Cart";
@@ -15,13 +16,13 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartShown && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
