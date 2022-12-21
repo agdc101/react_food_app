dddeb9bce69b68533fbab34d7486ec784b370c9b
agdc101 <agdc101@gmail.com>
2022-12-21 10:33:59 +0000

    content fix

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -6,12 +6,7 @@ const MealsSummary = () => {
     <section className={classes.summary}>
       <h2>Delicious Food, Delivered To You</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
-      </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+        The fabric of Ash's Takeaway was born out of love and respect for these humble deli creations, met with a desire to bring quality ingredients to the table. Simply put, we’re here to bring you a sandwich experience you can feel good about.
       </p>
     </section>
   );
