b4d8850e894f360eeef88b131c3c2b7a27598a46
agdc101 <agdc101@gmail.com>
2023-06-20 12:32:01 +0100

    data nouw pulled in from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import Card from '../Layout/Card';
 import MealItem from './MealItem/MealItem';
 import classes from '../Meals/AvailableMeals.module.css';
@@ -33,7 +33,28 @@ const DUMMY_MEALS = [
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const getMeals = async () => {
+        const response = await fetch('https://react-http-test-5443a-default-rtdb.firebaseio.com/Meals.json')
+          const responseData = await response.json();
+          const loadedMeals = [];
+          for (const key in responseData) {
+            loadedMeals.push({
+              id: key,
+              name: responseData[key].name,
+              description: responseData[key].description,
+              price: responseData[key].price
+            });
+          }
+          setMeals(loadedMeals);
+    }
+    getMeals();
+  },[]);
+
+
+    const mealsList = meals.map(meal => {
        return (
         <Card>
           <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>
